70928d88026ff3f9f0dd6055f8271e7263f21f19
Javier Jaramillo <javier.jaramillo2225@gmail.com>
2023-08-08 23:09:52 -0400

    Updated index.js to add a message form to DOM with remove button on it.

--- js/index.js
+++ js/index.js
@@ -17,3 +17,38 @@ for(i of skills){
     skill.innerHTML = i;
     skillsList.appendChild(skill);
 }
+
+const messageForm = document.getElementById('leave_message');
+
+messageForm.addEventListener('submit',(event) => {
+
+userName=event.target.usersName; 
+userEmail=event.target.usersEmail;
+userMessage=event.target.usersMessage;   
+console.log(userName);
+console.log(userEmail);
+console.log(userMessage);
+event.preventDefault();
+
+const messageSection = document.getElementById('messages');
+const messageList = messageSection.querySelector('ul');
+const newMessage = document.createElement('li');
+
+
+newMessage.innerHTML = `<a href=mailto:${userEmail.value}>${userName.value}</a>`+" wrote: "+`<span>${userMessage.value}</span>`;
+
+removeButton = document.createElement('button');
+removeButton.textContent = "Remove";
+removeButton.type = "button";
+removeButton.addEventListener('click',() => {
+const entry=removeButton.parentNode; 
+entry.remove();
+
+});
+
+newMessage.appendChild(removeButton); 
+messageList.appendChild(newMessage);
+messageSection.appendChild(messageList);
+
+messageForm.reset();
+});
