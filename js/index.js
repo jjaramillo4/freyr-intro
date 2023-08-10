855e9dce00b1f1014f6714c3133bded0d541911d
Javier Jaramillo <javier.jaramillo2225@gmail.com>
2023-08-09 21:44:39 -0400

    made revisions on assignment

--- js/index.js
+++ js/index.js
@@ -21,21 +21,23 @@ for(i of skills){
 const messageForm = document.getElementById('leave_message');
 
 messageForm.addEventListener('submit',(event) => {
+event.preventDefault();
+let userName=event.target.usersName.value; 
+let userEmail=event.target.usersEmail.value;
+let userMessage=event.target.usersMessage.value;
 
-userName=event.target.usersName; 
-userEmail=event.target.usersEmail;
-userMessage=event.target.usersMessage;   
+ 
 console.log(userName);
 console.log(userEmail);
 console.log(userMessage);
-event.preventDefault();
+
 
 const messageSection = document.getElementById('messages');
 const messageList = messageSection.querySelector('ul');
 const newMessage = document.createElement('li');
 
 
-newMessage.innerHTML = `<a href=mailto:${userEmail.value}>${userName.value}</a>`+" wrote: "+`<span>${userMessage.value}</span>`;
+newMessage.innerHTML = `<a href=mailto:${userEmail}>${userName}</a> wrote: <span>${userMessage}</span>`;
 
 removeButton = document.createElement('button');
 removeButton.textContent = "Remove";
